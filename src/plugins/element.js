2ed636bda5199e06ce852cafbcea188a0b11bf77
Salt <a1229821405@163.com>
2020-08-08 10:06:38 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -15,7 +15,12 @@ Option,
 Cascader,
 Alert,
 Tabs,
-TabPane
+TabPane,
+Steps,
+Step,
+CheckboxGroup,
+Checkbox,
+Upload
 } from 'element-ui'
 
 Vue.use(Button)
@@ -44,6 +49,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 
 
 
